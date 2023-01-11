f63661815b3422790af01d9ea733c7b27cb92838
Novante <78790665+Novante@users.noreply.github.com>
2023-01-11 20:56:04 +0100

    Initial commit

---
+++ backend-restapi/src/main/java/com/novante/backendrestapi/BackendRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.novante.backendrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRestapiApplication.class, args);
+    }
+
+}
---
+++ backend-restapi/src/test/java/com/novante/backendrestapi/BackendRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novante.backendrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
