78deecbb0518e7ac1f8eb439a4bbb49de6a3a669
Novante <78790665+Novante@users.noreply.github.com>
2023-01-12 01:01:19 +0100

    Added Homepage.js and Homepage.css. Added Ant Design sidemenu to Homepage.js.

---
+++ frontend/src/pages/Homepage/Homepage.js
@@ -0,0 +1,76 @@
+import {Button, Layout, Menu} from "antd";
+import './Homepage.css'
+
+import {
+    AppstoreOutlined,
+    ContainerOutlined,
+    DesktopOutlined,
+    MailOutlined,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    PieChartOutlined,
+} from '@ant-design/icons';
+import {useState} from "react";
+
+const Homepage = () => {
+
+    const [collapsed, setCollapsed] = useState(false);
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+    };
+
+    function getItem(label, key, icon, children, type) {
+        return {
+            key,
+            icon,
+            children,
+            label,
+            type,
+        };
+    }
+
+    const items = [
+        getItem('Option 1', '1', <PieChartOutlined/>),
+        getItem('Option 2', '2', <DesktopOutlined/>),
+        getItem('Option 3', '3', <ContainerOutlined/>),
+        getItem('Navigation One', 'sub1', <MailOutlined/>, [
+            getItem('Option 5', '5'),
+            getItem('Option 6', '6'),
+            getItem('Option 7', '7'),
+            getItem('Option 8', '8'),
+        ]),
+        getItem('Navigation Two', 'sub2', <AppstoreOutlined/>, [
+            getItem('Option 9', '9'),
+            getItem('Option 10', '10'),
+            getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
+        ]),
+    ];
+
+    return (
+        <div className="wrapper">
+            <div className="homepageMenu"
+            >
+                <Button
+                    type="primary"
+                    onClick={toggleCollapsed}
+                    style={{
+                        marginBottom: 16,
+                    }}
+                >
+                    {collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
+                </Button>
+                <Menu
+                    style={{height: '100vh'}}
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    mode="inline"
+                    theme="dark"
+                    inlineCollapsed={collapsed}
+                    items={items}
+                />
+            </div>
+        </div>
+    )
+
+}
+export default Homepage;
